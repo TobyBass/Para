ef60d5edd07e2b5208599bf9b7345a6902a9d5a3
TobyBass <TobyBass@users.noreply.github.com>
2016-08-03 15:08:59 -0400

    Add files via upload

---
+++ parser.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+
+std::vector<std::string> Parse(std::std::vector<std::string> a){
+	std::std::vector<std::string> ret;
+	for (int i = 0; i < a.size(); ++i)
+	{
+		
+	}
+	return ret;
+}
