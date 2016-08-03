28aad23a093ff269cd71890ceda6083122449e92
TobyBass <TobyBass@users.noreply.github.com>
2016-08-03 16:53:54 -0400

    Update parser.cpp

--- Parser/parser.cpp
+++ Parser/parser.cpp
@@ -8,31 +8,12 @@ int inhandle(std::string){
 	
 } 
 
-
 std::vector<std::string> Parse(std::vector<std::string> a){
 	std::vector<std::string> ret;
-	std::vector<std::string> varnames;
-	std::vector<const char*> types;
-	std::vector<int> invar;
 	
 	for (int i = 0; i < a.size(); ++i)
 	{
-		if(a.at(i) == "if"){
-			ret.push_back("IF");
-			++i;
-			if(a.at(i)  == "true" || a.at(i)  == "false"){
-				ret.push_back("C");
-			}else{
-				inhandle(a.at(i));
-				if(doerr){
-					for(int b = 0; b < varnames.size(); ++b){
-						if(a.at(i) == varnames.at(b)){
-							
-						}
-					}
-				}
-			}
-		}
+	
 	}
 	return ret;
 }
