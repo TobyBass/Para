cda01778c22ec22d6dc70da9e495eea638dcc7b9
TobyBass <TobyBass@users.noreply.github.com>
2016-08-07 19:59:12 -0400

    Update Macro.cpp

--- Parser/Macro.cpp
+++ Parser/Macro.cpp
@@ -1,1 +1,14 @@
-//
+#include <string>
+#include <vector>
+
+std::vector<std::string> Macro(std::vector<std::string> a){
+	for(size_t I = 0; I < a.size(); ++I){
+		if(a.at(I) == "@"){
+			switch(a.at(I)){
+				case:
+				
+				break;
+			}	
+		}
+  }
+}
