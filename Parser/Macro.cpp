e2c9d9444ad951e629d57d7a9bce5c00c9a3492c
TobyBass <TobyBass@users.noreply.github.com>
2016-08-07 21:05:15 -0400

    Update Macro.cpp

--- Parser/Macro.cpp
+++ Parser/Macro.cpp
@@ -2,13 +2,20 @@
 #include <vector>
 
 std::vector<std::string> Macro(std::vector<std::string> a){
+	std::vector<std::string> defnames;
+	std::vector<std::string> defdat;
+	bool ranis = false;
 	for(size_t I = 0; I < a.size(); ++I){
 		if(a.at(I) == "@"){
 			++I;
 			switch(a.at(I)){
 				case "define":
 				++I;
-				
+				for(size_t i = 0; i < defnames.size(); ++i){
+					if(a.at(I) == defnames.at(i)){
+						
+					}
+				}
 				break;
 				
 				case "if":
