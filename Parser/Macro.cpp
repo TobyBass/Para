80fa49edf96f457c30ec495bfcef64bded8b5026
TobyBass <TobyBass@users.noreply.github.com>
2016-08-07 20:07:14 -0400

    Update Macro.cpp

--- Parser/Macro.cpp
+++ Parser/Macro.cpp
@@ -4,8 +4,18 @@
 std::vector<std::string> Macro(std::vector<std::string> a){
 	for(size_t I = 0; I < a.size(); ++I){
 		if(a.at(I) == "@"){
+			++I;
 			switch(a.at(I)){
-				case:
+				case "define":
+				++I;
+				
+				break;
+				
+				case "if":
+				
+				break;
+				
+				case "endif":
 				
 				break;
 			}	
